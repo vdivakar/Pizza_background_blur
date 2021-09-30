e94cfcd61c614d1a827304018792062b9bdcd547
Divakar Verma <dv347@ilab1.cs.rutgers.edu>
2021-09-30 16:02:00 -0400

    Multiple instances will be saved as different files keeping rest blurred.

---
+++ src/mask_rcnn_images.py
@@ -0,0 +1,117 @@
+'''
+Notes:
+input_dir /common/home/dv347/Data/Pizza10/images
+out_dir /common/home/dv347/Data/Pizza10/blurred_output
+reference: https://debuggercafe.com/instance-segmentation-with-pytorch-and-mask-r-cnn/
+'''
+
+
+import torch
+import torchvision
+import cv2
+import argparse, os
+from os import listdir
+from os.path import isfile, join
+
+from PIL import Image
+from utils import draw_segmentation_map, get_outputs
+from torchvision.transforms import transforms as transforms
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-i', '--input', 
+                    help='path to the input data')
+parser.add_argument('-d', '--input_dir', required=True, 
+                    help='path to the input data')
+parser.add_argument('-o', '--out_dir', required=True, 
+                    help='path to the output data')
+parser.add_argument('-t', '--threshold', default=0.94, type=float,
+                    help='score threshold for discarding detection')
+args = vars(parser.parse_args())
+
+# initialize the model
+model = torchvision.models.detection.maskrcnn_resnet50_fpn(pretrained=True, progress=True, 
+                                                           num_classes=91)
+# set the computation device
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+# load the modle on to the computation device and set to eval mode
+model.to(device).eval()
+
+# transform to convert the image to tensor
+transform = transforms.Compose([
+    transforms.ToTensor()
+])
+
+
+img_dir = args['input_dir']
+image_files = [f for f in listdir(img_dir) if isfile(join(img_dir, f))]
+
+error_files = []
+blurred_files = []
+not_blurred_files = []
+
+list_multi_pizza_images = [] # append num of multi-pizzas detected
+
+for image_name in image_files:
+    
+    try:
+        image_path = os.path.join(img_dir, image_name)
+        image = Image.open(image_path).convert('RGB')
+
+        orig_image = image.copy()
+
+        # transform the image
+        image = transform(image)
+        # add a batch dimension
+        image = image.unsqueeze(0).to(device)
+
+        masks, boxes, labels, scores = get_outputs(image, model, args['threshold'])
+        
+        assert len(masks) == len(scores)
+        if len(scores) > 0:
+
+            results = draw_segmentation_map(orig_image, masks, boxes, labels)
+        
+            if len(results) > 1:
+                for i, result in enumerate(results):
+                    name = image_name.split(".jpg")[0] + f"_{i}.png"
+                    save_path = os.path.join(args['out_dir'], name)
+                    cv2.imwrite(save_path, result)
+                list_multi_pizza_images.append(image_name)
+            else:
+                image_name = image_name.replace(".jpg", ".png")
+                save_path = os.path.join(args['out_dir'], image_name)
+                cv2.imwrite(save_path, results[0])
+                
+            blurred_files.append(image_name)
+
+        else:
+            not_blurred_files.append(image_name)
+            print("Not Blurred: ", image_name)
+        
+    except Exception as e:
+        print(e)
+        error_files.append(image_name)
+
+        
+with open("blurred_list.txt", "w") as file:
+    for x in blurred_files:
+        file.write(x + "\n")
+        
+with open("not_blurred_list.txt", "w") as file:
+    for x in not_blurred_files:
+        file.write(x + "\n")
+        
+with open("error_files.txt", "w") as file:
+    for x in error_files:
+        file.write(x + "\n")
+
+with open("multi_pizza_images.txt", "w") as file:
+    for x in list_multi_pizza_images:
+        file.write(x + "\n")
+        
+print("Finished!!")
+print("len(blurred_files) = ", {len(blurred_files)})
+print("len(not_blurred_list) = ", {len(not_blurred_files)})      
+print("len(error_files) = ", {len(error_files)})      
+print("Images with multiple pizzas: ", len(list_multi_pizza_images))
---
+++ src/utils.py
@@ -0,0 +1,71 @@
+import cv2
+import numpy as np
+import random
+import torch
+
+from coco_names import COCO_INSTANCE_CATEGORY_NAMES as coco_names
+
+# this will help us create a different color for each class
+# COLORS = np.random.uniform(0, 255, size=(len(coco_names), 3))
+
+def get_outputs(image, model, threshold):
+    with torch.no_grad():
+        # forward pass of the image through the modle
+        outputs = model(image)
+    
+    # get all the scores
+    scores = list(outputs[0]['scores'].detach().cpu().numpy())
+    
+    # get the classes labels
+    labels = [coco_names[i] for i in outputs[0]['labels']]
+    
+    # get the masks
+    masks = (outputs[0]['masks']>0.5).squeeze().detach().cpu().numpy()
+    
+    # get the bounding boxes, in (x1, y1), (x2, y2) format
+    boxes = [[(int(i[0]), int(i[1])), (int(i[2]), int(i[3]))]  for i in outputs[0]['boxes'].detach().cpu()]
+    
+    only_pizza_scores = []
+    only_pizza_labels = []
+    only_pizza_masks = []
+    only_pizza_boxes = []
+    
+    for idx, label in enumerate(labels):
+        if label == 'pizza':
+            only_pizza_scores.append(scores[idx])
+            only_pizza_labels.append(labels[idx])
+            only_pizza_masks.append(masks[idx])
+            only_pizza_boxes.append(boxes[idx])
+
+    only_pizza_masks = np.array(only_pizza_masks)
+
+    # index of those scores which are above a certain threshold
+    thresholded_preds_inidices = [i for i in only_pizza_scores if i > threshold]
+    thresholded_preds_count = len(thresholded_preds_inidices)
+    
+    # discard masks for objects which are below threshold
+    only_pizza_masks = only_pizza_masks[:thresholded_preds_count]
+    # discard bounding boxes below threshold value
+    only_pizza_boxes = only_pizza_boxes[:thresholded_preds_count]
+    only_pizza_labels = only_pizza_labels[:thresholded_preds_count]
+    only_pizza_scores = only_pizza_scores[:thresholded_preds_count]
+    
+    return only_pizza_masks, only_pizza_boxes, only_pizza_labels, only_pizza_scores
+
+def draw_segmentation_map(image, masks, boxes, labels):
+    results = []
+    image = np.array(image)
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+    all_blur = cv2.blur(image, (50,50), 0)
+    for i in range(len(masks)):
+        # add unblurred pizzas iteratively to the all_blur image
+        #all_blur[masks[i]>0] = image[masks[i]>0]
+        
+        # Changing logic:- Now, each image should have only 1 pizza.
+        # Un-blur 1 pizza at a time and keep rest blurred.
+        temp = all_blur.copy()
+        temp[masks[i]>0] = image[masks[i]>0]
+        results.append(temp)
+    
+    return results
+
